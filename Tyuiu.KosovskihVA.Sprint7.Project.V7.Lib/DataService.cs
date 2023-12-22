ff6b446a5c688baa280d25597417d5f04b9370dd
Tyuiu.kosovskihVA@outlook.com <mr.kosovskih35@mail.ru>
2023-12-22 19:04:11 +0500

    Add project files.

---
+++ Tyuiu.KosovskihVA.Sprint7.Project.V7.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KosovskihVA.Sprint7.Project.V7.Lib
+{
+    public class DataService
+    {
+
+    }
+}
